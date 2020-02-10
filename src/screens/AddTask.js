3587e23467a9e0ce32c0d1c9466631de263be2b3
Vinicyus Camopestrini <vinicyus.campes@gmail.com>
2020-02-10 14:22:36 -0300

    aula 89

--- src/screens/AddTask.js
+++ src/screens/AddTask.js
@@ -14,6 +14,16 @@ export default class AddTask extends Component {
         ...initialState
     }
 
+    save = () => {
+        const newTask ={
+            desc: this.state.desc,
+            date: this.state.date
+        }
+
+        this.props.onSave && this.props.onSave(newTask)
+        this.setState({ ...initialState })
+    }
+
     getDatePicker = () => {
         let datePicker = <DateTimePicker value={this.state.date}
             onChange={(_, date) => this.setState({date, showDatePicker: false})}
@@ -61,7 +71,7 @@ export default class AddTask extends Component {
                             <Text style={styles.button}>Cancelar</Text>
                         </TouchableOpacity>
 
-                        <TouchableOpacity>
+                        <TouchableOpacity onPress={this.save}>
                             <Text style={styles.button}>Salvar</Text>
                         </TouchableOpacity>
                     </View>
--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import { View, Text, ImageBackground, StyleSheet, FlatList, TouchableOpacity, Platform } from 'react-native'
+import { View, Text, ImageBackground, StyleSheet, FlatList, TouchableOpacity, Platform, Alert } from 'react-native'
 import commonStyles from '../commonStyles'
 import todayImage from '../../assets/imgs/today.jpg'
 import Icon from 'react-native-vector-icons/FontAwesome'
@@ -58,13 +58,31 @@ export default class TaskList extends Component {
         this.setState({ tasks }, this.filterTasks)
     }
 
+    addTask = newTask => {
+        if(!newTask.desc || !newTask.desc.trim()) {
+            Alert.alert('DadosInvalidos', 'Descrição não informada')
+            return 
+        }
+
+        const tasks = [...this.state.tasks]
+        tasks.push({
+            id: Math.random(),
+            desc: newTask.desc,
+            estimateAt: newTask.date,
+            doneAt: null
+        })
+
+        this.setState({tasks, showAddTask: false}, this.filterTasks)
+    }
+
 
     render() {
         const today = moment().locale('pt-br').format('ddd, D [de] MMMM')
         return (
             <View style={styles.conteiner}>
                 <Addtask isVisible={this.state.showAddTask}
-                    onCancel={() => this.setState({showAddTask: false})}/>
+                    onCancel={() => this.setState({showAddTask: false})}
+                    onSave={this.addTask}/>
                 <ImageBackground source={todayImage}
                     style={styles.background}>
                     <View style={styles.iconBar}>
